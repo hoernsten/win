de879d58a3c50fdbbfd2e63923af799e37dfe2aa
David Hörnsten <david.hoernsten@gmail.com>
2019-11-01 00:06:08 +0100

    Add files via upload

---
+++ clean.ps1
@@ -0,0 +1,13 @@
+﻿Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+Remove-Item "C:\Windows\Temp\*" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "$env:USERPROFILE\AppData\Local\Temp\*" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Recent\*" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"-ErrorAction SilentlyContinue
+
+Write-EventLog –LogName Application –Source “Script” –EntryType Information –EventID 0 –Message (
+
+    "Ran custom script.`n",
+    "Time: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')",
+    "File: $($MyInvocation.MyCommand.Name)",
+    "Path: $(Split-Path $MyInvocation.MyCommand.Definition)" | Out-String
+)

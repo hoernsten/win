b89796aece9152844bda71ee6a8d2e1974465a78
David Hörnsten <david.hoernsten@gmail.com>
2019-11-11 15:48:56 +0100

    Update thx.ps1

--- thx.ps1
+++ thx.ps1
@@ -1,85 +1,106 @@
-# Check if the AudioDeviceCmdlets module is installed
-if (-Not (Get-Command Get-AudioDevice -ErrorAction SilentlyContinue)) {
+# Run as administrator
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 
-    # Download and import the AudioDeviceCmdlets module
-    Write-Host "Importing AudioDeviceCmdlets..."
-    Set-Location $env:USERPROFILE\Downloads
-    Invoke-WebRequest -URI https://github.com/frgnca/AudioDeviceCmdlets/releases/download/v3.0/AudioDeviceCmdlets.dll -OutFile AudioDeviceCmdlets.dll
+    Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+}
 
-    # Verify the file hash before importing
-    if ((Get-FileHash AudioDeviceCmdlets.dll).Hash -eq "7DD88E4467A2455313AA15DA755B88EBD1F9A8F456301AEA8D369514FD9A5E64") {
+else {
 
-        New-Item "$($profile | Split-Path)\Modules\AudioDeviceCmdlets" -Type Directory -Force
-        Copy-Item AudioDeviceCmdlets.dll "$($profile | Split-Path)\Modules\AudioDeviceCmdlets\AudioDeviceCmdlets.dll"
-        Set-Location "$($profile | Split-Path)\Modules\AudioDeviceCmdlets"
-        Get-ChildItem | Unblock-File
-        Import-Module AudioDeviceCmdlets
-        Remove-Item $env:USERPROFILE\Downloads\AudioDeviceCmdlets.dll
+    # Create an event log
+    Write-EventLog â€“LogName Application â€“Source â€œScriptâ€ â€“EntryType Information â€“EventID 0 â€“Message (
+
+        "Ran custom script.`n",
+        "Time: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')",
+        "File: $($MyInvocation.MyCommand.Name)",
+        "Path: $(Split-Path $MyInvocation.MyCommand.Definition)" | Out-String
+    )
+
+    # Check if the AudioDeviceCmdlets module is installed
+    if (-Not (Get-Command Get-AudioDevice -ErrorAction SilentlyContinue)) {
+
+        # Download and import the AudioDeviceCmdlets module
+        Write-Host "Importing AudioDeviceCmdlets..."
+        Set-Location $env:USERPROFILE\Downloads
+        Invoke-WebRequest -URI https://github.com/frgnca/AudioDeviceCmdlets/releases/download/v3.0/AudioDeviceCmdlets.dll -OutFile AudioDeviceCmdlets.dll
+
+        # Verify the file hash before importing
+        if ((Get-FileHash AudioDeviceCmdlets.dll).Hash -eq "7DD88E4467A2455313AA15DA755B88EBD1F9A8F456301AEA8D369514FD9A5E64") {
+
+            New-Item "$($profile | Split-Path)\Modules\AudioDeviceCmdlets" -Type Directory -Force
+            Copy-Item AudioDeviceCmdlets.dll "$($profile | Split-Path)\Modules\AudioDeviceCmdlets\AudioDeviceCmdlets.dll"
+            Set-Location "$($profile | Split-Path)\Modules\AudioDeviceCmdlets"
+            Get-ChildItem | Unblock-File
+            Import-Module AudioDeviceCmdlets
+            Remove-Item $env:USERPROFILE\Downloads\AudioDeviceCmdlets.dll
+        }
+
+        else {
+
+            Remove-Item AudioDeviceCmdlets.dll
+            Write-Host "Error: File hash does not match!" -ForegroundColor Red
+            Write-Host "Press Enter to quit..."; Read-Host
+            Exit
+        }
     }
 
-    else {
+    # Switch playback device
+    try {
 
-        Remove-Item AudioDeviceCmdlets.dll
-        Write-Host "Error: File hash does not match!" -ForegroundColor Red
-        Write-Host "Press Enter to quit..."
-        Read-Host
+        Set-AudioDevice -ID $((Get-AudioDevice -List | Where-Object {$_.Name -eq "HÃ¶gtalare (Razer Kraken TE - Game)"}).ID) 1> $null
+    }
+
+    catch [System.ArgumentException] {
+
+        Write-Host "Error: No playback device named 'HÃ¶gtalare (Razer Kraken TE - Game)' found" -ForegroundColor Red
+        Write-Host "Press Enter to quit... " -NoNewline; Read-Host
         Exit
     }
+
+
+    # Stop the THX service
+    Write-Host "Stopping the THX service..."
+    Stop-Service THXService -ErrorAction SilentlyContinue
+
+    # Stop the THX Helper processes
+    Get-Process -ProcessName THXHelper* | foreach {
+
+        Write-Host "Restarting the" $_.Name "process..."
+        Stop-Process -Name $_.Name
+    }
+
+    Sleep 2
+
+    # Start the THX Helper processes
+    Get-ChildItem -Recurse 'C:\Program Files (x86)\Razer\' -File THXHelper*.exe | foreach {
+
+        Start-Process -FilePath $_.FullName
+    }
+
+    # Start the THX service
+    Write-Host "Starting the THX service..."
+    Start-Service THXService -ErrorAction SilentlyContinue
+
+    Sleep 2
+
+    # Switch back to previous playback device
+    try {
+
+        Set-AudioDevice -ID $((Get-AudioDevice -List | Where-Object {$_.Name -eq "HÃ¶gtalare (THX Spatial - Synapse)"}).ID) 1> $null
+    }
+
+    catch [System.ArgumentException] {
+
+        Write-Host "Error: No playback device named 'HÃ¶gtalare (THX Spatial - Synapse)' found" -ForegroundColor Red
+        Write-Host "Press Enter to quit... " -NoNewline; Read-Host
+        Exit
+    }
+
+    # Keep switching between the playback devices until audio starts working again
+    While ($true) {
+
+        Write-Host "Press Enter (Ctrl+C to abort)... " -NoNewline; Read-Host
+        Set-AudioDevice -ID $((Get-AudioDevice -List | Where-Object {$_.Name -eq "HÃ¶gtalare (Razer Kraken TE - Game)"}).ID) 1> $null
+        Sleep 1
+        Set-AudioDevice -ID $((Get-AudioDevice -List | Where-Object {$_.Name -eq "HÃ¶gtalare (THX Spatial - Synapse)"}).ID) 1> $null
+    }
 }
-
-# Switch playback device
-try {
-
-    Set-AudioDevice -ID $((Get-AudioDevice -List | Where-Object {$_.Name -eq "Högtalare (Razer Kraken TE - Game)"}).ID) 1> $null
-}
-
-catch [System.ArgumentException] {
-
-    Write-Host "Error: No playback device named 'Högtalare (Razer Kraken TE - Game)' found" -ForegroundColor Red
-}
-
-
-# Stop the THX service
-Write-Host "Stopping the THX service..."
-Stop-Service THXService -ErrorAction SilentlyContinue
-
-# Stop the THX Helper processes
-Get-Process -ProcessName THXHelper* | foreach {
-
-    Write-Host "Restarting the" $_.Name "process..."
-    Stop-Process -Name $_.Name
-}
-
-Sleep 2
-
-# Start the THX Helper processes
-Get-ChildItem -Recurse 'C:\Program Files (x86)\Razer\' -File THXHelper*.exe | foreach {
-
-    Start-Process -FilePath $_.FullName
-}
-
-# Start the THX service
-Write-Host "Starting the THX service..."
-Start-Service THXService -ErrorAction SilentlyContinue
-
-Sleep 2
-
-# Switch back to previous playback device
-try {
-
-    Set-AudioDevice -ID $((Get-AudioDevice -List | Where-Object {$_.Name -eq "Högtalare (THX Spatial - Synapse)"}).ID) 1> $null
-}
-
-catch [System.ArgumentException] {
-
-    Write-Host "Error: No playback device named 'Högtalare (THX Spatial - Synapse)' found" -ForegroundColor Red
-}
-
-# Create an event log
-Write-EventLog –LogName Application –Source “Script” –EntryType Information –EventID 0 –Message (
-
-    "Ran custom script.`n",
-    "Time: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')",
-    "File: $($MyInvocation.MyCommand.Name)",
-    "Path: $(Split-Path $MyInvocation.MyCommand.Definition)" | Out-String
-)
